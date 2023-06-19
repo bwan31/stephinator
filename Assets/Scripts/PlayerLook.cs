d7f934092fb44cd1a10822de2a4ed504071c0721
bwan31 <bwan70707@gmail.com>
2023-06-19 14:09:27 -0400

    made sensitivity BUTTERY SMOOTH & organized better

--- Assets/Scripts/PlayerLook.cs
+++ Assets/Scripts/PlayerLook.cs
@@ -7,18 +7,18 @@ public class PlayerLook : MonoBehaviour
     private float xRotation = 0f;
     public Camera cam;
 
-    public float xSensitivity = 30f;
-    public float ySensitivity = 30f;
+    public float xSensitivity = .1f;
+    public float ySensitivity = .1f;
     
     public void ProcessLook(Vector2 input) {
         float mouseX = input.x;
         float mouseY = input.y;
         // calculate camera rotatinon for looking up and down
-        xRotation -= (mouseY * Time.deltaTime) * ySensitivity;
+        xRotation -= (mouseY) * ySensitivity; // * Time.deltaTime
         xRotation = Mathf.Clamp(xRotation, -80f, 80f);
-        //applythis to our camera transform
+        //apply this to our camera transform
         cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
-        //rotate player to lok left and right
-        transform.Rotate(Vector3.up * (mouseX * Time.deltaTime) * xSensitivity);
+        //rotate player to look left and right
+        transform.Rotate(Vector3.up * (mouseX) * xSensitivity); // * Time.deltaTime
     }
 }
