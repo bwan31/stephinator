5a02d9459ed1faf533f6e6a3cba21625cf04be96
Zenoson <zenoson1234@gmail.com>
2023-06-18 22:02:21 -0400

    slide maybe

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class PlayerMotor : MonoBehaviour
 {
+    CapsuleCollider collider;
+    float originalHeight;
+    public float reducedHeight;
+    private bool sliding;
+    Rigidbody rig;
+    private float timer; 
+
     private CharacterController controller;
     private Vector3 playerVelocity;
     public float speed = 10f; // make sure this value is equal to line 62 
@@ -24,11 +31,14 @@ public class PlayerMotor : MonoBehaviour
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        collider = GetComponent<CapsuleCollider>();
+        originalHeight = collider.height;
+        rig = GetComponent<Rigidbody>();
     }
-
+   
     // Update is called once per frame
     void Update()
-    {        
+    {   
         isGrounded = controller.isGrounded;
         if(lerpCrouch) { 
             crouchTimer += Time.deltaTime;
@@ -36,6 +46,9 @@ public class PlayerMotor : MonoBehaviour
             p *= p;
             if(crouching || Input.GetKeyDown(KeyCode.LeftControl)) {
                 // if (speed > 10) call slide
+                if (speed>maxSpeed*.8) {
+                    Sliding();
+                }
                 controller.height = Mathf.Lerp(controller.height, 1, p);
                 speed = 4f;
                 Debug.Log(speed);
@@ -51,28 +64,49 @@ public class PlayerMotor : MonoBehaviour
 
     }
 
+    public void Sliding() {
+        sliding = true;
+        collider.height = reducedHeight;
+        rig.AddForce(transform.forward * speed * 3, ForceMode.VelocityChange);
+
+        StartCoroutine(ExecuteAfterTime(1));
+    }
+
+    IEnumerator ExecuteAfterTime(float time) {
+        yield return new WaitForSeconds(time);
+        
+        GoUp();
+    }
+    
+    public void GoUp() {
+        collider.height = originalHeight;
+        speed = speed/2;
+        sliding = false;
+    }
+
     //recieve the inputs for our InputManager.cs and apply them to our character controller.
     public void ProcessMove(Vector2 input) {
-        Vector3 moveDirection = Vector3.zero;
-        if(speed < maxSpeed && crouching != true){
-            speed += acceleration * Time.deltaTime;
+        if (true) {
+            Vector3 moveDirection = Vector3.zero;
+            if(speed < maxSpeed && crouching != true){
+                speed += acceleration * Time.deltaTime;
+            }
+
+            //moveDirection.x = moveDirection.x + speed*Time.deltaTime;
+
+            moveDirection.x = input.x;
+            moveDirection.z = input.y;
+            if ( transform.TransformDirection(moveDirection) * speed * Time.deltaTime == Vector3.zero) 
+                speed = 10;
+            controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+
+
+            playerVelocity.y += gravity * Time.deltaTime;
+            if (isGrounded && playerVelocity.y < 0)
+                playerVelocity.y = -2f;
+            controller.Move(playerVelocity * Time.deltaTime);
+            //Debug.Log(playerVelocity.y);
         }
-
-        //moveDirection.x = moveDirection.x + speed*Time.deltaTime;
-
-        moveDirection.x = input.x;
-        moveDirection.z = input.y;
-        if ( transform.TransformDirection(moveDirection) * speed * Time.deltaTime == Vector3.zero) 
-            speed = 10;
-        controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
-
-
-        playerVelocity.y += gravity * Time.deltaTime;
-        if (isGrounded && playerVelocity.y < 0)
-            playerVelocity.y = -2f;
-        controller.Move(playerVelocity * Time.deltaTime);
-        //Debug.Log(playerVelocity.y);
-
     }
     public void Jump() {
         if(isGrounded) {
